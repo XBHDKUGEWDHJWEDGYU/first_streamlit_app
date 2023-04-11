6e02f04aeff45df08ead370c920fc30ce5133b91
XBHDKUGEWDHJWEDGYU <130075303+XBHDKUGEWDHJWEDGYU@users.noreply.github.com>
2023-04-11 16:46:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-
+my_fruit = my_fruit.set_index('Fruit')
 streamlit.text('Build your own smoothie')
 my_fruit=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
